98bd69235153d3ee29fd35b52d12f523b5513502
Eduardo Rebelo <eduldrebelo@gmail.com>
2021-05-12 15:55:24 -0300

    First commit

---
+++ src/main/java/com/erebelo/evirtual/EvirtualApplication.java
@@ -0,0 +1,13 @@
+package com.erebelo.evirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erebelo/evirtual/EvirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erebelo.evirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

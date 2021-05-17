f50eca07ee7045618609d0ba91bdcb42cbe3a34f
Eduardo Rebelo <eduldrebelo@gmail.com>
2021-05-17 15:32:19 -0300

    Insert a new Category through POST

--- src/main/java/com/erebelo/evirtual/resources/CategoryResource.java
+++ src/main/java/com/erebelo/evirtual/resources/CategoryResource.java
@@ -1,11 +1,15 @@
 package com.erebelo.evirtual.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.erebelo.evirtual.domain.Category;
 import com.erebelo.evirtual.services.CategoryService;
@@ -21,5 +25,12 @@ public class CategoryResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Category obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }

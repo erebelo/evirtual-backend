2dd4d6ec44ab221670d56c07bd3f1470744a9da8
Eduardo Rebelo <eduldrebelo@gmail.com>
2021-06-07 19:26:32 -0300

    Spring security initial configuration

--- src/main/java/com/erebelo/evirtual/resources/CustomerOrderResource.java
+++ src/main/java/com/erebelo/evirtual/resources/CustomerOrderResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.erebelo.evirtual.domain.Category;
 import com.erebelo.evirtual.domain.CustomerOrder;
-import com.erebelo.evirtual.dto.CategoryDTO;
 import com.erebelo.evirtual.services.CustomerOrderService;
 
 @RestController
